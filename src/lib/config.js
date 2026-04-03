75e62685c34d5c0ea57bd39eb0b5bdd2000feb93
Syifa Dwiky Basamala <119727680+syifadwikyb@users.noreply.github.com>
2026-04-03 20:47:44 +0700

    update

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,2 +1,3 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://backend-bustracking.onrender.com";
+// export const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://backend-bustracking.onrender.com";
 // export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://145.79.15.182:5000";
